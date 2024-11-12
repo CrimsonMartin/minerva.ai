caaafb825e8d63fe236946e322dd7fde3feaf11b
CrimsonMartin <maf152@case.edu>
2024-11-11 23:11:45 -0600

    Add Tailwind CSS integration and update project structure

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./templates/**/*.html",
+  "./static/src/**/*.js"
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
